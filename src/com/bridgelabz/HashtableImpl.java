f9cdfae2c3e3e250059387a91da8d779f2b56532
mayavathi <mayachandre27@gmail.com>
2021-07-04 02:59:32 -0400

    Remove word from hashtable

--- src/com/bridgelabz/HashtableImpl.java
+++ src/com/bridgelabz/HashtableImpl.java
@@ -1,7 +1,6 @@
 package com.bridgelabz;
 
 import java.util.ArrayList;
-
 public class HashtableImpl<K, V> {
     Node head;
     Node tail;
@@ -73,6 +72,24 @@ public class HashtableImpl<K, V> {
         int index = hashCode % numOfBuckets;
         //System.out.println("Key: "+word+" hashcode: "+hashCode+" index: "+index);
         return index;
+    }
+    //Remove "avoidable" from hashtable
+    public void remove(K word) {
+        Node currentNode = head;
+        Node previousNode = null;
+        while (currentNode != null && currentNode.getKey().equals(word)) {
+            head = currentNode.getNext();
+            return;
+        }
+        while (currentNode != null && !(currentNode.getKey().equals(word))) {
+            previousNode = currentNode;
+            currentNode = currentNode.getNext();
+        }
+        if (currentNode != null) {
+            previousNode.next = currentNode.next;
+        }
+        if(currentNode == null)
+            System.out.println("Word not found!");
     }
 
     //Print the linked list
--- src/com/bridgelabz/HashtableMain.java
+++ src/com/bridgelabz/HashtableMain.java
@@ -1,24 +1,36 @@
 package com.bridgelabz;
 
+import java.util.Hashtable;
+
 public class HashtableMain {
 
-	    public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 	        HashtableImpl<String, Integer> hashImpl = new HashtableImpl();
-	        String message = "To be or not to be";
+	        String message = "Paranoids are not paranoid because they are paranoid but " +
+	                "because they keep putting themselves deliberately into paranoid avoidable situations";
 
 	        //String to String[] conversion.
+	   
 	        String[] messageArray = message.toLowerCase().split(" ");
 
 	        //Iterate within the array
 	        for(String word: messageArray) {
 	            Integer value =  hashImpl.get(word);
+	          
 	            if( value == null)
 	                value = 1;
 	            else
 	                value = value + 1;
-	            hashImpl.add(word , value);
+	              hashImpl.add(word , value);
 	        }
-	        //Display the linked list
+	        //Display the hashtable
+	        System.out.println(hashImpl);
+
+	        //Remove "avoidable" from the hashtable
+	        hashImpl.remove("avoidable");
+
+	        //Display the hashtable
 	        System.out.println(hashImpl);
 	    }
 	}
